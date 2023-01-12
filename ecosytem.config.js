e47c3620b3ec1d1c5a52d8d8ae4b2250fbb8aad0
Rugved <rugved@phpdots.com>
2023-01-12 19:18:07 +0530

    added config

--- ecosytem.config.js
+++ ecosytem.config.js
@@ -1,14 +1,23 @@
-module.exports = [
-  (module.exports = {
-    apps: [
-      {
-        name: "nextapp",
-        script: "npm",
-        args: "start",
-        env: {
-          NODE_ENV: "production",
-        },
+module.exports = {
+  apps: [
+    {
+      name: "nextpublic",
+      script: "npm",
+      args: "start",
+      //   instances: 4,
+      autorestart: true,
+      watch: true,
+      //   max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
       },
-    ],
-  }),
-];
+      env_production: {
+        NODE_ENV: "production",
+      },
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    },
+  ],
+};
