af8f1d3378e4b3445fbae61381de992feeb8cd41
Rugved <rugved@phpdots.com>
2023-01-19 11:27:47 +0530

    animation added

--- src/constants/policies.ts
+++ src/constants/policies.ts
@@ -481,6 +481,7 @@ processing of personal data as described in this notice affecting you
 can be viewed on the EMFI website at
 https://www.emfi.uk/english/privacy-policy.
 </p>`;
+
 export const termofUseES = `  <p>
 EMFI appreciates your visit to this web site ("EMFI Website") and your
 interest in our services and products. Your privacy is important to us
@@ -698,214 +699,113 @@ https://www.emfi.uk/english/privacy-policy.
 
 export const privacypolicyES = `
 <p>
-EMFI appreciates your visit to this web site ("EMFI Website") and your
-interest in our services and products. Your privacy is important to us
-and we want you to feel comfortable visiting our site. We take care to
-protect your personal data collected, processed and used during visits
-to the EMFI Website.
+EMFI agradece su visita a este sitio web ("Sitio web de EMFI") y su interés en nuestros servicios y productos. Su privacidad es importante para nosotros y queremos que se sienta cómodo visitando nuestro sitio. Nos encargamos de proteger sus datos personales recopilados, procesados y utilizados durante las visitas al sitio web de EMFI.
 </p>
 <p>
-How we protect or use your data varies from country to country.
-Privacy Notices can be found for certain jurisdictions where EMFI
-entities process personal data. The terms of this Policy will be
-subject to the relevant jurisdiction-specific Privacy Notice where
-applicable. Please click on the jurisdiction that applies to you from
-the list at https://www.emfi.uk/english/privacy-policy
+La forma en que protegemos o usamos sus datos varía de un país a otro. Se pueden encontrar avisos de privacidad para ciertas jurisdicciones donde las entidades de EMFI procesan datos personales. Los términos de esta Política estarán sujetos al Aviso de Privacidad correspondiente a la jurisdicción correspondiente, donde corresponda. Por favor, haga clic en la jurisdicción que se aplica a usted de la lista en https://www.emfi.uk/espanol/privacy-policy
 </p>
 <h4>
-By accessing the EMFI Website you accept this Privacy Policy
-("Policy").
+Al acceder al sitio web de EMFI, acepta esta Política de privacidad ("Política").
 </h4>
 <p>
-If you do not agree to this Policy, do not proceed to further web
-pages of the EMFI Website.
+Si no está de acuerdo con esta Política, no continúe con otras páginas web del sitio web de EMFI.
 </p>
-<h4>1. What are the purpose and the scope of this Policy?</h4>
+<h4>1. ¿Cuáles son el propósito y el alcance de esta Política?</h4>
 <p>
-EMFI Group, its subsidiaries and branches ("EMFI" or "we") are
-committed to safeguarding the personal information that we collect
-from individuals who use the EMFI Website. Accordingly, EMFI has
-developed this Policy to describe how and what type of data will be
-collected from users of the EMFI Website and the purposes for which
-EMFI may collect, share or disclose the data. EMFI also outlines
-reasonable precautions which are taken to keep your data secure.
+El Grupo EMFI, sus subsidiarias y sucursales ("EMFI" o "nosotros") estamos comprometidos a proteger la información personal que recopilamos de las personas que utilizan el sitio web de EMFI. En consecuencia, EMFI ha desarrollado esta Política para describir cómo y qué tipo de datos se recopilarán de los usuarios del sitio web de EMFI y los fines para los cuales EMFI puede recopilar, compartir o divulgar los datos. EMFI también describe las precauciones razonables que se toman para mantener sus datos seguros.
 </p>
 <p>
-This Policy applies to any information obtained by EMFI through your
-use of the EMFI Website. It is not applicable to any Internet websites
-controlled by third parties not affiliated with EMFI that the EMFI
-Website may link to ("Third Party Sites"). Please review the privacy
-policies of Third Party Sites as EMFI is not responsible for and has
-no influence on the content or the privacy practices of Third Party
-Sites.
+Esta Política se aplica a cualquier información obtenida por EMFI a través de su uso del sitio web de EMFI. No se aplica a los sitios web de Internet controlados por terceros no afiliados a EMFI a los que el sitio web de EMFI puede enlazar ("Sitios de terceros"). Revise las políticas de privacidad de los Sitios de terceros, ya que EMFI no es responsable y no tiene influencia en el contenido o las prácticas de privacidad de los Sitios de terceros.
 </p>
 <p>
-The terms of this Policy are subject to any additional terms of
-disclaimers or other contractual terms you have entered into with EMFI
-such as client privacy policies or notices, and any applicable
-mandatory laws and regulations.
+Los términos de esta Política están sujetos a cualquier término adicional de renuncias de responsabilidad u otros términos contractuales que haya firmado con EMFI, como las políticas de privacidad del cliente o avisos, y las leyes y regulaciones obligatorias aplicables.
 </p>
 <h4>
-2. What information do we collect from you and for what purpose?
+2. ¿Qué información recopilamos de usted y con qué propósito?
 </h4>
 <p>
-When you visit the EMFI Website, our web server automatically records
-details about your visit (for example, your IP address, the web site
-from which you visit us, the type of browser software used, the EMFI
-Website pages that you actually visit including the date and the
-duration of your visit).
+Cuando visita el sitio web de EMFI, nuestro servidor web registra automáticamente los detalles de su visita (por ejemplo, su dirección IP, el sitio web desde el que nos visita, el tipo de software de navegador utilizado, las páginas del sitio web de EMFI que realmente visita, incluido el fecha y la duración de su visita).
 </p>
 <p>
-In addition, we collect personal data which you provide through the
-EMFI Website, for example when you enter personal details (e.g. name,
-gender, address, e-mail address, phone/fax number) on a registration
-page or if you sign up for an email newsletter.
+Además, recopilamos datos personales que nos proporciona a través del sitio web de EMFI, por ejemplo, cuando ingresa datos personales (por ejemplo, nombre, sexo, dirección, dirección de correo electrónico, número de teléfono / fax) en una página de registro o si se registra en un boletín electrónico.
 </p>
-<p>EMFI will use your personal data:</p>
+<p>EMFI utilizará sus datos personales:
+
+</p>
 <ul>
 <li>
-  for the purposes of technical administration and research &
-  development of the EMFI Website,
+A los fines de administración técnica, investigación y desarrollo del sitio web de EMFI,
 </li>
-<li>for customer and user administration and marketing,</li>
-<li>to inform you about our services and products, and</li>
-<li>for such purposes as otherwise specified.</li>
+<li>Para la administración y comercialización de clientes y usuarios.</li>
+<li>Para informarle sobre nuestros servicios y productos, y</li>
+<li>para los fines especificados de otra manera.</li>
 </ul>
 <p>
-EMFI respects applicable laws and regulations in its use of personal
-data.
+EMFI respeta las leyes y regulaciones aplicables en el uso de datos personales.
 </p>
-<h4>3. How do we collect and store information from you?</h4>
+<h4>3. ¿Cómo recopilamos y almacenamos su información?</h4>
 <p>
-EMFI uses tracking technology such as cookies or tags to gather
-information as outlined above (see: "What information do we collect
-from you and for what purpose?") to understand how visitors use the
-EMFI Website.
+EMFI utiliza tecnología de rastreo como cookies o etiquetas para recopilar la información como se describe anteriormente (consulte: "¿Qué información recopilamos de usted y con qué propósito?") Para comprender cómo los visitantes utilizan el sitio web de EMFI.
 </p>
 <p>
-Tracking technology helps us manage and improve the usability of the
-EMFI Website, for example by detecting whether there has been any
-contact between your computer and us in the past and to identify the
-most popular sections of the EMFI Website.
+La tecnología de seguimiento nos ayuda a administrar y mejorar la usabilidad del sitio web de EMFI, por ejemplo, al detectar si ha habido algún contacto entre su computadora y nosotros en el pasado e identificar las secciones más populares del sitio web de EMFI.
 </p>
 <p>
-Where available, the “Cookies Policy” link at the bottom of the page
-gives you more details about EMFI’s use of cookies, and lets you
-control which types of cookies the EMFI Website may set on your device
-(known as your “cookie settings”). Please ensure that you set your
-cookie settings by un-ticking certain types of cookies if you do not
-consent to their use by EMFI.
+Cuando esté disponible, el enlace "Política de cookies" en la parte inferior de la página le brinda más detalles sobre el uso de cookies de EMFI y le permite controlar qué tipos de cookies puede establecer el sitio web de EMFI en su dispositivo (conocidas como "configuraciones de cookies") . Asegúrese de establecer la configuración de cookies desactivando ciertos tipos de cookies si no está de acuerdo con su uso por parte de EMFI.
 </p>
 <p>
-When you save your cookie settings, they should also apply to your
-future visits to the EMFI Website. However, for technical reasons
-beyond EMFI’s control, this cannot be guaranteed. For example, if you
-reset your browser, delete your cookies or access the EMFI website
-from another browser or device your cookie settings may be lost. To
-comply with applicable laws and regulations, in some countries you may
-be asked to confirm your cookie settings when you first visit the EMFI
-Website. If you are in a country where you are automatically required
-to set your cookie settings, you may be asked to set them again on a
-future visit.
+Cuando guarde su configuración de cookies, también deberían aplicarse a sus futuras visitas al sitio web de EMFI. Sin embargo, por razones técnicas fuera del control de EMFI, esto no se puede garantizar. Por ejemplo, si reinicia su navegador, borra sus cookies o accede al sitio web de EMFI desde otro navegador o dispositivo, su configuración de cookies puede perderse. Para cumplir con las leyes y regulaciones aplicables, en algunos países se le puede solicitar que confirme la configuración de sus cookies cuando visite por primera vez el sitio web de EMFI. Si se encuentra en un país en el que se le solicita automáticamente que establezca la configuración de las cookies, es posible que se le solicite que las vuelva a configurar en una futura visita.
 </p>
 <p>
-In many cases you can also control tracking technologies using your
-browser. Please ensure that your browser setting reflects whether you
-wish to be warned about and/or accept tracking technologies (such as
-cookies) where possible. The specific capabilities of your browser and
-instructions on how to use them can usually be found in the manual or
-help file of your browser.
+En muchos casos, también puede controlar las tecnologías de seguimiento utilizando su navegador. Asegúrese de que la configuración de su navegador refleje si desea que se le avise y / o acepte tecnologías de seguimiento (como cookies) cuando sea posible. Las capacidades específicas de su navegador y las instrucciones sobre cómo usarlas generalmente se pueden encontrar en el manual o en el archivo de ayuda de su navegador.
 </p>
 <p>
-Refusing, disabling or deactivating of tracking technologies may
-result in a reduced availability of the services provided by the EMFI
-Website or parts of the EMFI Website may no longer function correctly.
+Rechazar, deshabilitar o desactivar las tecnologías de seguimiento puede resultar en una disponibilidad reducida de los servicios proporcionados por el sitio web de EMFI o es posible que algunas partes del sitio web de EMFI ya no funcionen correctamente.
 </p>
 <h4>
-4. To whom do we disclose your information collected from the EMFI
-Website?
+4. ¿A quién revelamos su información recopilada del sitio web de EMFI?
 </h4>
 <p>
-EMFI may disclose your information to its affiliates and their agents
-and third-party providers inside or outside your country of residence
-to perform services for EMFI and for the purposes stated above. Our
-affiliates, agents and third-party providers who have access to
-personal data obtained through the EMFI Website are obliged to respect
-privacy.
+EMFI puede divulgar su información a sus afiliados y sus agentes y proveedores externos dentro o fuera de su país de residencia para realizar servicios para EMFI y para los fines indicados anteriormente. Nuestros afiliados, agentes y proveedores externos que tienen acceso a los datos personales obtenidos a través del sitio web de EMFI están obligados a respetar la privacidad.
 </p>
 <p>
-When transferring personal data internationally we make sure that we
-comply with applicable laws and regulations, for example, by entering
-into agreements which will ensure that the recipients of your
-information maintain an adequate level of data protection.
+Al transferir datos personales a nivel internacional, nos aseguramos de que cumplimos con las leyes y regulaciones aplicables, por ejemplo, mediante la celebración de acuerdos que asegurarán que los destinatarios de su información mantengan un nivel adecuado de protección de datos.
 </p>
 <p>
-We may also disclose your information to governmental agencies or
-entities, regulatory authorities, or other persons in line with any
-applicable law, regulations, court order or official request, or under
-any and for the purposes of any guidelines issued by regulatory or
-other authorities, or similar processes as either required or
-permitted by applicable law.
+También podemos divulgar su información a agencias o entidades gubernamentales, autoridades reguladoras u otras personas de acuerdo con cualquier ley, reglamento, orden judicial o solicitud oficial aplicable, o bajo cualquiera y para los fines de las pautas emitidas por autoridades regulatorias o de otro tipo, o procesos similares según lo requiera o lo permita la ley aplicable
 </p>
 <h4>
-5. What security measures have we implemented to protect your
-information collected through the EMFI Website?
+5. ¿Qué medidas de seguridad hemos implementado para proteger su información recopilada a través del sitio web de EMFI?
 </h4>
 <p>
-EMFI has implemented reasonable technical and organisational security
-measures to protect your personal data collected by EMFI via the EMFI
-Website against unauthorized access, misuse, loss or destruction.
+EMFI ha implementado medidas de seguridad organizativas y técnicas razonables para proteger sus datos personales recopilados por EMFI a través del sitio web de EMFI contra accesos no autorizados, uso indebido, pérdida o destrucción.
 </p>
-<h4>6. How do we treat electronic messages sent to and from EMFI?</h4>
+<h4>6. ¿Cómo tratamos los mensajes electrónicos enviados hacia y desde EMFI?</h4>
 <p>
-All electronic messages sent to and from EMFI are automatically
-retained in a journaling system which preserves the evidential weight
-of the e-mails. They are protected by reasonable technical and
-organisational measures and may only be accessed in justified cases in
-line with applicable laws and regulations (e.g. court order, suspicion
-of criminal conduct, violation of regulatory obligations, material
-breach of employment contract) by specific persons in defined
-functions (e.g. Legal, Compliance, Risk). All emails are disposed of
-after the applicable retention period has expired.
+Todos los mensajes electrónicos enviados hacia y desde EMFI se retienen automáticamente en un sistema de registro por diario que conserva el peso probatorio de los correos electrónicos. Están protegidos por medidas técnicas y organizativas razonables y solo se puede acceder a ellos en casos justificados de acuerdo con las leyes y regulaciones aplicables (por ejemplo, orden judicial, sospecha de conducta criminal, violación de obligaciones reglamentarias, incumplimiento sustancial del contrato laboral) por personas específicas Funciones (ej. Legal, Cumplimiento, Riesgo). Todos los correos electrónicos se eliminan después de que el período de retención aplicable haya expirado.
 </p>
 <h4>
-7. What should you consider when sending data over the Internet?
+7. ¿Qué debe considerar al enviar datos a través de Internet?
 </h4>
 <p>
-The Internet is generally not regarded as a secure environment, and
-information sent via the Internet (such as to or from the EMFI Website
-or via electronic message) may be accessed by unauthorized third
-parties, potentially leading to disclosures, changes in content or
-technical failures. Even if both sender and receiver are located in
-the same country, information sent via the Internet may be transmitted
-across international borders and be forwarded to a country with a
-lower data protection level than exists in your country of residence.
+En general, no se considera que Internet sea un entorno seguro, y terceros no autorizados pueden acceder a la información que se envía a través de Internet (por ejemplo, desde o hacia el sitio web de EMFI) o mediante mensajes electrónicos. . Incluso si el remitente y el destinatario se encuentran en el mismo país, la información enviada a través de Internet puede transmitirse a través de las fronteras internacionales y enviarse a un país con un nivel de protección de datos más bajo que el que existe en su país de residencia.
 </p>
 <h4>
-8. How do we deal with information from individuals under the age of
-18?
+8. ¿Cómo tratamos la información de personas menores de 18 años?
 </h4>
 <p>
-The EMFI Website does not seek to collect personal data from
-individuals under the age of 18. Individuals under the age of 18
-should receive permission from their parent or legal guardian before
-providing any personal data to EMFI on the EMFI Website.
+El sitio web de EMFI no pretende recopilar datos personales de personas menores de 18 años. Las personas menores de 18 años deben recibir el permiso de sus padres o tutores legales antes de proporcionar datos personales a EMFI en el sitio web de EMFI.
 </p>
-<h4>9. How can you access or review your personal data?</h4>
-<li>check whether we hold your personal data,</li>
-<li>ask us to provide you with a copy of your personal data, or</li>
+<h4>9. ¿Cómo puedes acceder o revisar tus datos personales?</h4>
+<li>Compruebe si conservamos sus datos personales,</li>
+<li>pedirnos que le proporcionemos una copia de sus datos personales, o</li>
 <li>
-require us to correct any of your personal data that is inaccurate.
+Exigirnos que corrijamos cualquiera de sus datos personales que sea inexacto.
 </li>
 <p>
-Should you have a request regarding the processing of your personal
-data please send an email to the following address: compliance@emfi.uk
+Si tiene una solicitud con respecto al procesamiento de sus datos personales, envíe un correo electrónico a la siguiente dirección: compliance@emfi.uk
 </p>
-<h4>10. Status of this privacy notice</h4>
+<h4>10. Estado de este aviso de privacidad</h4>
 <p>
-This privacy notice was updated in December 2018. We reserve the right
-to amend it from time to time. Any future changes or additions to the
-processing of personal data as described in this notice affecting you
-can be viewed on the EMFI website at
-https://www.emfi.uk/english/privacy-policy.
+Este aviso de privacidad se actualizó en diciembre de 2018. Nos reservamos el derecho de modificarlo de vez en cuando. Cualquier cambio o adición futura al procesamiento de datos personales como se describe en este aviso que le afecta puede verse en el sitio web de EMFI en https://www.emfi.uk/english/privacy-policy.
 </p>`;
